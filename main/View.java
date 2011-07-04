fd4b946f56470ed48e28131c2d7483fa73f3fd96
Raido Kalbre <RKalbre@gmail.com>
2011-07-04 16:59:19 +0300

    method for getting comments

--- main/View.java
+++ main/View.java
@@ -1,11 +1,14 @@
 package main;
 
+import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.URL;
+import java.util.List;
 
 import com.google.gson.Gson;
 
+import main.RedditData.Child;
 import main.RedditData.ChildData;
 import main.RedditData.Data;
 
@@ -15,6 +18,22 @@ import main.RedditData.Data;
 public class View {
 
    private View() {
+   }
+
+   public static void singlePostComments(RedditData[] data) {
+      System.out.println(data.length + " COMMENTS");
+      for (int i = 1; i < data.length; i++) {
+         commentLevel(data[i].getData().getChildren(), " ");
+      }
+   }
+   
+   private static void commentLevel(List<Child> data, String padding) {
+      for (int i = 1; i < data.size(); i++) {
+         System.out.println(padding + data.get(i).getData().getBody());
+         if (data.get(i).getData().getReplies() != null) {
+            commentLevel(data.get(i).getData().getReplies().getData().getChildren(), padding + " ");
+         }
+      }
    }
 
    public static void singlePost(ChildData data) {
@@ -32,17 +51,21 @@ public class View {
          System.out.println(offset+i+1 + ". " + child.getTitle().replace("\n", "") + " (" + child.getDomain() + ")");
       }
    }
-
-   public static Data getData(String topic, int count, String after) {
+   
+   public static Data getPosts(String topic, int count, String after) {
       Gson gson = new Gson();
-      RedditData reddit = null;
-      String additional = "";
-      if (!"".equals(after)) additional = "&after=" + after;
-      reddit = gson.fromJson(getJSONData("http://www.reddit.com/" + topic.trim() + "/.json?limit=" + count + additional), RedditData.class);
+      if (!"".equals(after)) after = "&after=" + after;
+      RedditData reddit = gson.fromJson(getJSONData("http://www.reddit.com/" + topic.trim() + "/.json?limit=" + count + after, false), RedditData.class);
       return reddit.getData();
    }
+   
+   public static RedditData[] getComments(String post, int count) {
+      Gson gson = new Gson();
+      RedditData[] reddit = gson.fromJson(getJSONData("http://www.reddit.com/" + post + "comments/.json?limit=" + count, true), RedditData[].class);
+      return reddit;
+   }
 
-   private static InputStreamReader getJSONData(String address){
+   private static InputStreamReader getJSONData(String address, boolean debug){
       InputStreamReader reader = null;
       try {
          URL url = new URL(address);
@@ -52,19 +75,21 @@ public class View {
       }
       
       // FOR DEBUGGING
-//      BufferedReader in = new BufferedReader(reader);
-//      String text = null;
-//      try {
-//         if ((text = in.readLine()) != null) {
-//            System.out.println(text.replace("{", "{\n")
-//                                    .replace("}", "}\n")
-//                                    .replace("[", "[\n")
-//                                    .replace("]", "]\n")
-//                                    .replace(", ", ",\n"));
-//         }
-//      } catch (IOException e) {
-//         e.printStackTrace();
-//      }
+      if (debug) {
+         BufferedReader in = new BufferedReader(reader);
+         String text = null;
+         try {
+            if ((text = in.readLine()) != null) {
+               System.out.println(text.replace("{", "{\n")
+                     .replace("}", "}\n")
+                     .replace("[", "[\n")
+                     .replace("]", "]\n")
+                     .replace(", ", ",\n"));
+            }
+         } catch (IOException e) {
+            e.printStackTrace();
+         }
+      }
       // END OF DEBUGGING PART
       
       return reader;
